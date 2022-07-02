7d7dc78f92713aa8b1e72dc12418b9949842acb8
golesuman <gole35830@gmail.com>
2022-07-02 13:06:24 +0545

    updated

--- Basics/emailvalidationusingStrings.py
+++ Basics/emailvalidationusingStrings.py
@@ -2,9 +2,9 @@ email = input('Enter your email')
 k = 0
 j = 0
 d = 0
-if len(email)>6:
+if len(email)> 6:
     if  email[0].isalpha():
-        if ("@" in email) and (email.count('@')== 1):
+        if ("@" in email) and (email.count('@') == 1):
             if (email[-4] == '.') ^ (email[-3] == '.'): # returns false it both are true and 
                 #returns true if both are opposite
                 for char in email:
